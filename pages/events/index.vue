23f70cea9f0a24738f527e23604da56203c67b3d
Jason Anton <rev.jt.anton@gmail.com>
2020-03-08 11:49:37 -0400

    Moving around elements and fixing icons for Netlify

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -10,56 +10,46 @@
     </div>
 
     <div class="container" id="upcoming-events">
-      <div class="row py-4">
-        <div class="col-sm-12 py-2 my-auto">
-          <h2 class="display-4">Hack Nights and What Happens</h2>
-          <p>Project nights where our members work on select projects to benefit Baltimore communities.</p>
-          <p>If you're new, no worries! You can easily jump in wherever your skills are needed.</p>
-          <h2 class="display-4">Community Nights</h2>
-          <p>Networking events with members of the community and the group to discuss issues that could be solved with civic tech.</p>
-        </div>
-      </div>
-
       <div v-if="events !== undefined">
         <div class="row py-2">
           <div class="col-sm-12 py-2 my-auto">
             <h3 class="display-4">Upcoming Events:</h3>
           </div>
-            <vl-map
-              :load-tiles-while-animating="true"
-              :load-tiles-while-interacting="true"
-              :controls="{zoom:false}"
-              style="height: 400px"
-            >
-              <vl-view
-                :zoom.sync="zoom"
-                :center.sync="center"
-                :rotation.sync="rotation"
-                projection="EPSG:4326"
-              ></vl-view>
+          <vl-map
+            :load-tiles-while-animating="true"
+            :load-tiles-while-interacting="true"
+            :controls="{zoom:false}"
+            style="height: 400px"
+          >
+            <vl-view
+              :zoom.sync="zoom"
+              :center.sync="center"
+              :rotation.sync="rotation"
+              projection="EPSG:4326"
+            ></vl-view>
 
-              <vl-layer-tile class="osm">
-                <vl-source-osm></vl-source-osm>
-              </vl-layer-tile>
+            <vl-layer-tile class="osm">
+              <vl-source-osm></vl-source-osm>
+            </vl-layer-tile>
 
-              <vl-feature class="point">
-                <vl-geom-point :coordinates="[-76.6072, 39.28954]"></vl-geom-point>
-                <vl-style-box>
-                  <vl-style-icon src="./images/marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
-                </vl-style-box>
-              </vl-feature>
+            <vl-feature class="point">
+              <vl-geom-point :coordinates="[-76.6072, 39.28954]"></vl-geom-point>
+              <vl-style-box>
+                <vl-style-icon src="/images/marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
+              </vl-style-box>
+            </vl-feature>
 
-              <vl-geoloc>
-                <template slot-scope="geoloc">
-                  <vl-feature v-if="geoloc.position" id="position-feature">
-                    <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
-                    <vl-style-box>
-                      <vl-style-icon src="./images/flag.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
-                    </vl-style-box>
-                  </vl-feature>
-                </template>
-              </vl-geoloc>
-            </vl-map>
+            <vl-geoloc>
+              <template slot-scope="geoloc">
+                <vl-feature v-if="geoloc.position" id="position-feature">
+                  <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
+                  <vl-style-box>
+                    <vl-style-icon src="/images/flag.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
+                  </vl-style-box>
+                </vl-feature>
+              </template>
+            </vl-geoloc>
+          </vl-map>
         </div>
         <div v-for="(event, index) in events" :key="index">
           <div class="row py-2">
@@ -85,20 +75,27 @@
             <hr />
           </div>
         </div>
-
-        <div class="row py-2">
-          <div class="col-sm-12 py-2">
-            <h3>
-              Get notified of our next event:
-              <a
-                href="http://www.meetup.com/Code-for-Baltimore/events/"
-                target="_blank"
-              >Join our Meetup group</a>
-            </h3>
-          </div>
+      </div>
+      <div class="row py-4">
+        <div class="col-sm-12 py-2 my-auto">
+          <h2 class="display-4">Hack Nights and What Happens</h2>
+          <p>Project nights where our members work on select projects to benefit Baltimore communities.</p>
+          <p>If you're new, no worries! You can easily jump in wherever your skills are needed.</p>
+          <h2 class="display-4">Community Nights</h2>
+          <p>Networking events with members of the community and the group to discuss issues that could be solved with civic tech.</p>
         </div>
       </div>
-      <div v-else></div>
+      <div class="row py-2">
+        <div class="col-sm-12 py-2">
+          <h3>
+            Get notified of our next event:
+            <a
+              href="http://www.meetup.com/Code-for-Baltimore/events/"
+              target="_blank"
+            >Join our Meetup group</a>
+          </h3>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -108,6 +105,23 @@ import contentful from "~/plugins/contentful.js";
 import VueLayers from "vuelayers";
 import "vuelayers/lib/style.css"; // needs css-loader
 import moment from "moment";
+
+const locationPins = async events => {
+  let locations = [];
+
+  for (var event of events) {
+    if (locations[event.fields.venueName] !== undefined) {
+      locations[event.fields.venueName].dates.push(event.fields.date);
+    } else {
+      locations[event.fields.venueName] = {
+        geolocation: [event.fields.location.lon, event.fields.location.lat],
+        dates: [event.fields.date]
+      };
+    }
+  }
+
+  return locations;
+};
 
 export default {
   components: {
@@ -119,19 +133,12 @@ export default {
       order: "fields.title",
       include: 5
     });
-
     // Making the location OL/OSM compatable for ease of use...
-    for (var i = 0; i < events.items.length; i++) {
-      events.items[i].fields.location = [
-        events.items[i].fields.location.lon,
-        events.items[i].fields.location.lat
-      ];
-      console.log(events.items[i].fields.location)
-    }
-
+    const locations = await locationPins(events.items);
     return {
       events: events.items,
-      zoom: 12,
+      locations: locations,
+      zoom: 13,
       rotation: 0,
       center: [-76.6072, 39.28954]
     };
