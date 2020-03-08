f2209044f51bded9af28f68a03b059889e71a11c
Jason Anton <rev.jt.anton@gmail.com>
2020-03-07 20:54:44 -0500

    fixed moving pin and added geoloc

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -4,7 +4,7 @@
     <div class="jumbotron jumbotron-events">
       <div class="container">
         <div class="d-block flex-row text-center py-1">
-          <h1 class="display-4">Hack Nights, Community Nights and more</h1>
+          <h1 class="display-4">Hack Nights, Community Nights and more!</h1>
         </div>
       </div>
     </div>
@@ -25,6 +25,41 @@
           <div class="col-sm-12 py-2 my-auto">
             <h3 class="display-4">Upcoming Events:</h3>
           </div>
+            <vl-map
+              :load-tiles-while-animating="true"
+              :load-tiles-while-interacting="true"
+              :controls="{zoom:false}"
+              style="height: 400px"
+            >
+              <vl-view
+                :zoom.sync="zoom"
+                :center.sync="center"
+                :rotation.sync="rotation"
+                projection="EPSG:4326"
+              ></vl-view>
+
+              <vl-layer-tile class="osm">
+                <vl-source-osm></vl-source-osm>
+              </vl-layer-tile>
+
+              <vl-feature class="point">
+                <vl-geom-point :coordinates="[-76.6072, 39.28954]"></vl-geom-point>
+                <vl-style-box>
+                  <vl-style-icon src="./images/marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
+                </vl-style-box>
+              </vl-feature>
+
+              <vl-geoloc>
+                <template slot-scope="geoloc">
+                  <vl-feature v-if="geoloc.position" id="position-feature">
+                    <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
+                    <vl-style-box>
+                      <vl-style-icon src="./images/flag.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
+                    </vl-style-box>
+                  </vl-feature>
+                </template>
+              </vl-geoloc>
+            </vl-map>
         </div>
         <div v-for="(event, index) in events" :key="index">
           <div class="row py-2">
@@ -35,7 +70,8 @@
               >{{ $moment(event.fields.date).format('MMMM Do YYYY, h:mm a') }}</h4>
               <h3>{{ event.fields.title }}</h3>
               <p class="text-muted">
-                <i class="fas fa-map-marker-alt"></i> Spark Baltimore
+                <i class="fas fa-map-marker-alt"></i>
+                {{ event.fields.venueName }}
               </p>
               <div v-html="$md.render(event.fields.description)"></div>
 
@@ -44,32 +80,7 @@
               </a>
             </div>
           </div>
-          <div>
-            <vl-map
-              :controls="controls"
-              :load-tiles-while-animating="true"
-              :load-tiles-while-interacting="true"
-              style="height: 400px"
-            >
-              <vl-view
-                :zoom.sync="zoom"
-                :center.sync="event.fields.location"
-                :rotation.sync="rotation"
-                projection="EPSG:4326"
-              ></vl-view>
 
-              <vl-layer-tile id="osm">
-                <vl-source-osm></vl-source-osm>
-              </vl-layer-tile>
-
-              <vl-feature v-if="event.fields.location" id="position-feature">
-                <vl-geom-point :coordinates="event.fields.location"></vl-geom-point>
-                <vl-style-box>
-                  <vl-style-icon src="/images/marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
-                </vl-style-box>
-              </vl-feature>
-            </vl-map>
-          </div>
           <div class="col-sm-12 py-2 my-auto">
             <hr />
           </div>
@@ -115,13 +126,14 @@ export default {
         events.items[i].fields.location.lon,
         events.items[i].fields.location.lat
       ];
+      console.log(events.items[i].fields.location)
     }
 
     return {
       events: events.items,
-      zoom: 15,
+      zoom: 12,
       rotation: 0,
-      controls: false
+      center: [-76.6072, 39.28954]
     };
   }
 };
